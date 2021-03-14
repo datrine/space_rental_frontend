d8e56e96051bd1b88c8cef8e0b886ca2a598275f
datrine <trinitietp@gmail.com>
2021-03-14 09:39:49 +0100

    pretransfer to flash

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
 //import { Button, Col, Container, Row } from 'react-bootstrap';
-import { Comp_Mob_Header} from "../comps/general/comp_mob_menu"
-import { Comp_Mob_Footer} from "../comps/general/comp_mob_footer"
+import { Comp_Mob_Header } from "../comps/general/comp_mob_menu"
+import { Comp_Mob_Footer } from "../comps/general/comp_mob_footer"
 import { PCMenu } from '../comps/general/comp_pc_menu';
 import { Comp_CustomerChatApp } from '../comps/general/comp_chat_app';
+import { Container } from '@material-ui/core';
 const DynamicPCComp = dynamic(() => Promise.resolve(PCView));
 const DynamicMobileComp = dynamic(() => Promise.resolve(MobileView));
 
@@ -24,36 +25,49 @@ let screenMgr = () => {
 }
 
 export default function Home() {
-  let { screenType } = screenMgr() || {};
-  let indexView = null;
-  switch (screenType) {
-      case "small":
-          indexView = <DynamicMobileComp />
-          break;
+    let { screenType } = screenMgr() || {};
+    let indexView = null;
+    switch (screenType) {
+        case "small":
+            indexView = <DynamicMobileComp />
+            break;
 
-      case "large":
-          indexView = <DynamicPCComp />
-          break;
-      default:
-          indexView = <>Loading...</>
-          break;
-  }
-  return <>
-      {indexView}
-  </>
+        case "large":
+            indexView = <DynamicPCComp />
+            break;
+        default:
+            indexView = <>Loading...</>
+            break;
+    }
+    return <>
+        {indexView}
+    </>
 }
 
 let PCView = () => {
     return <>
-    <PCMenu/>
-    <Comp_CustomerChatApp/>
+        <PCMenu />
+        <Comp_CustomerChatApp />
     </>
 }
 
 let MobileView = () => {
     return <>
-    <Comp_Mob_Header/>
-    <Comp_Mob_Footer/>
+        <Comp_Mob_Header />
+        <IndexBody />
+        <Comp_Mob_Footer />
+    </>
+}
+
+let IndexBody = () => {
+    return <>
+        <Container maxWidth className="mt-5 pt-5" >
+            <Container maxWidth>
+                <h1 className="w3-text-white" style={{ backgroundColor: "rgba(0,0,0,0.5)",
+                paddingRight:"10",paddingLeft:"10",textAlign:"center" }} >
+                    Find Amazing Tenants, Apartments & Investments</h1>
+            </Container>
+        </Container>
     </>
 }
 
