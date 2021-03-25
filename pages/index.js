8e02229565193eae650c0eaa0c3a2c9ece2cca0f
datrine <trinitietp@gmail.com>
2021-03-25 04:35:25 +0100

    Wale's promise

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ let MobileView = () => {
     }, [])
     return <>
         {readyState ? <>
-            <Comp_Mob_Header />
+        <Comp_Mob_Header/>
             <IndexBody />
             <Comp_Mob_Footer />
         </> : <SplashScreen />}
@@ -45,14 +45,23 @@ let MobileView = () => {
 
 let IndexBody = () => {
     return <>
-        <Container maxWidth className="mt-5" style={{
+    <Container  className="mt-5" style={{
             padding: 0,
-            height: 700, backgroundImage: "url('hp_share_apart.png')", backgroundRepeat: "no-repeat",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center", backgroundSize: "cover"
+        }}>
+        <Container style={{
+            padding: 0, 
+            height: 700, backgroundImage: "url('hp_share_apart.png')",
+            backgroundRepeat: "no-repeat",
             backgroundPosition: "center", backgroundSize: "cover"
         }} >
-            <Container maxWidth style={{ paddingTop: 100, height: 700, backgroundColor: "rgba(0,128,0,0.5)" }} >
+            <Container maxWidth="xs" style={{
+                paddingTop: 100, height: 700,
+                backgroundColor: "rgba(0,128,0,0.5)"
+            }} >
                 <h1 className="w3-text-white" style={{
-                    fontSize: "4em",
+                    fontSize: "2em",
                     paddingRight: "10", paddingLeft: "10", textAlign: "center"
                 }} >Share & Find Amazing Tenants,Rooms, Offices & Real Estate Investments</h1>
             </Container>
@@ -63,62 +72,62 @@ let IndexBody = () => {
         </Container>
         <Container maxWidth className="mb-5 pb-5" >
             <Tiles />
-            <QuickFind/>
+            <QuickFind />
         </Container>
-        <br />
-        <br />
+</Container>
     </>
 }
 
 let Tiles = () => {
     let listOf = [{
-        link:"/listings/rooms",
+        link: "/listings/rooms",
         titleOf: "Find a place to stay",
         textOf: "Entire homes, apartment, rooms & more",
         imgSrcOf: "/home_find.png"
     }, {
-        link:"/listings/office_spaces",
+        link: "/listings/office_spaces",
         titleOf: "Find a place to work",
         textOf: "Entire building, part of an office, & more",
         imgSrcOf: "/workspace_find.png"
     }, {
-        link:"/listings/investments",
+        link: "/listings/investments",
         titleOf: "Find a real estate investment",
         textOf: "long-term or short-term investment in real esteate space",
         imgSrcOf: "/investment_find.png"
     }, {
-        link:"/listings/tenants",
+        link: "/listings/tenants",
         titleOf: "Find a tenant for your space",
         textOf: "House, Office & more",
         imgSrcOf: "/tenant_find.png"
     },]
     return <>
-        {listOf.map(({link, titleOf, textOf, imgSrcOf }, index) => 
-        <a href={link} key={index}>
-        <Container className="w3-card"
-            style={{
-                width: "80%", margin: "10%", borderStyle: "solid",
-                borderWidth: 1, borderRadius: "20px", boxShadow: "10px 10px grey"
-            }} >
-            <Grid container
-                direction="row"
-                justify="space-between"
-                alignItems="center"
-            >
-                <Grid item xs={10} sm={11} >
-                    <h4 style={{ fontWeight: "bold" }}>
-                        {titleOf.length > 44 ? (titleOf.substring(0, 40) + "...") : titleOf}</h4>
-                    <p>{textOf}</p>
-                </Grid>
-                <Grid item xs={2} sm={1} direction="column" justify="flex-end" alignItems="center" >
-                    <img width={50} height={50} src={imgSrcOf} />
-                </Grid>
-            </Grid>
-        </Container>
-        </a>)}  </>
+        {listOf.map(({ link, titleOf, textOf, imgSrcOf }, index) =>
+            <a href={link} key={index} style={{textDecoration:"none"}} >
+                <Container className="w3-card"
+                    style={{
+                        width: "96%", marginLeft: "2%",marginRight:"2%",marginBottom:"15px", borderStyle: "solid",
+                        borderWidth: 1, borderRadius: "20px", boxShadow: "10px 10px grey"
+                    }} >
+                    <Grid container
+                        direction="row"
+                        justify="space-between"
+                        alignItems="center"
+                    >
+                        <Grid item xs={10} sm={11} >
+                            <h5 style={{ fontWeight: "bold" }}>
+                                {titleOf.length > 44 ? (titleOf.substring(0, 40) + "...") : titleOf}
+                                </h5>
+                            <p>{textOf}</p>
+                        </Grid>
+                        <Grid item xs={2} sm={1} direction="column" justify="flex-end" alignItems="center" >
+                            <img width={50} height={50} src={imgSrcOf} />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </a>)}  </>
 }
 
-let QuickFind=()=>{
-    return<>
+let QuickFind = () => {
+    return <>
     </>
 }
